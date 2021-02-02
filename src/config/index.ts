f28e418e2bb0466583bd5e5cec510bcb1f692d38
Ayman Aladdin <ayman.alaa1012@gmail.com>
2021-02-02 15:55:46 +0200

    add file for auth config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,12 +1,3 @@
-import dotenv from 'dotenv';
-
-// Set the NODE_ENV to 'development' by default
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-
-const envFound = dotenv.config();
-
-if (envFound.error) throw new Error("Couldn't find .env file");
-
 export * from './api';
 export * from './knex';
 export * from './mysql';
