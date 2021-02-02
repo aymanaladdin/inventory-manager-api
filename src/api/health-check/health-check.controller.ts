cc762da4e2323a6401aa32873f449ccb67546c3b
Ayman Aladdin <ayman.alaa1012@gmail.com>
2021-02-02 15:54:16 +0200

    add healthcheck route

--- src/api/health-check/health-check.controller.ts
+++ src/api/health-check/health-check.controller.ts
@@ -1,20 +1,28 @@
 import express, { Router } from "express";
+import { IController } from "../../loader/interfaces";
 
-export interface IControllerOptions {
-  prefix: string,
-  router: express.Router
+export class HealthCheckController implements IController {
+  private prefix: string;
+  private routes: express.Router;
+
+  constructor() {
+    this.prefix = '/health-check';
+    this.routes = Router();
+    this.registerRoutes();
+  }
+
+  public getRoutes(): express.Router {
+    return this.routes;
+  }
+
+  public getPrefix(): string {
+    return this.prefix;
+  }
+
+  private registerRoutes() {
+    this.routes.get('/', (req: express.Request, res: express.Response) => {
+      res.status(200).json({});
+    });
+  }
 }
 
-export function healthCheckController(mainRouter: express.Router): IControllerOptions {
-  const prefix = '/health-check';
-  const healthCheckRouter = Router();
-
-  healthCheckRouter.get('/', (req: express.Request, res: express.Response) => {
-    res.status(200).json({});
-  });
-
-  return {
-    prefix,
-    router: healthCheckRouter
-  };
-}
